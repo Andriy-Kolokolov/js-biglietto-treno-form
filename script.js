651e896639e749de10ebb56f3b8429d8699b8aa7
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-13 17:28:28 +0200

    add input-output checks

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ const discountJunior = 0.20; // 20%
 const discountSenior = 0.40; // 40%
 
 const btnGetPrice = document.getElementById("btn-calculate-price");
-const btnCancel = document.getElementById("btn-cancel");
+const btnReset = document.getElementById("btn-reset");
 const fieldTicketOwnerName = document.getElementById("output-ticket-owner-name");
 const fieldTicketPrice = document.getElementById("output-ticket-price");
 
 btnGetPrice.addEventListener("click", function (event) {
     event.preventDefault();
-    const inputName = document.getElementById("input-name").value;
-    const inputDistance = parseInt(document.getElementById("input-travel-distance").value);
-    const inputAge = document.getElementById("input-select-age").value;
+    if (!inputIsEmpty()) {
+        const inputName = document.getElementById("input-name").value;
+        const inputDistance = parseInt(document.getElementById("input-travel-distance").value);
+        const inputAge = document.getElementById("input-select-age").value;
 
-    fieldTicketPrice.innerText = getTravelPrice(inputAge, inputDistance) + " €";
-    fieldTicketOwnerName.innerText = inputName;
+        fieldTicketPrice.innerText = getTravelPrice(inputAge, inputDistance).toFixed(2) + " €";
+        fieldTicketOwnerName.innerText = inputName;
+    }
+    else {
+        alert("Fill all the fields to get ticket")
+    }
 });
 
-btnCancel.addEventListener("click", function (event) {
-    //todo
+btnReset.addEventListener("click", function (event) {
+    event.preventDefault();
+    cleanInputOutput();
 });
 
 function getTravelPrice(age, distance) {
-
     const defaultPrice = distance * kilometrePrice;
 
     if (age === "junior") {
@@ -32,4 +37,20 @@ function getTravelPrice(age, distance) {
     } else if (age === "senior") {
         return defaultPrice - defaultPrice * discountSenior;
     }
+}
+
+function cleanInputOutput() {
+    document.getElementById("input-name").value = "";
+    document.getElementById("input-travel-distance").value = "";
+    document.getElementById("input-select-age").value = "Select..";
+    document.getElementById("output-ticket-owner-name").innerText = "";
+    document.getElementById("output-ticket-price").innerText = "";
+}
+
+function inputIsEmpty() {
+    const name = document.getElementById("input-name").value;
+    const dist = document.getElementById("input-travel-distance").value;
+    const age = document.getElementById("input-select-age").value;
+
+    return (name.length === 0 || dist.length === 0 || age === "Select..");
 }
